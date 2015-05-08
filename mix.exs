c9dd8a8369000492240969ada2b763433768aafa
Toby Lawrence <tobias.lawrence@gmail.com>
2015-05-08 08:36:50 -0400

    Cutting version 0.0.5

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Elistrix.Mixfile do
 
   def project do
     [app: :elistrix,
-     version: "0.0.4",
+     version: "0.0.5",
      elixir: "~> 1.0",
      name: "Elistrix",
      description: description,
